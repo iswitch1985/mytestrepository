ad790f481ef767611693af62be691e043e5c1216
iswitch1985 <97214883+iswitch1985@users.noreply.github.com>
2022-04-14 12:10:14 +0800

    Update 1-if_else.js

--- 1-if_else.js
+++ 1-if_else.js
@@ -6,8 +6,10 @@ const amount = 12
 //this line is add from remote123123214xxx
 =======
 //this line is add from remote123123214
+
 >>>>>>> e6727c77fc0208ac90d247fe10140fa2ae92a832
 if (amount < 10) {
+//comment added
 console.log('small number')
 }
 else{

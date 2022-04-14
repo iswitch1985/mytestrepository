d3ba394bdb68e9eb996d185d5369427466655ecb
iswitch1985 <97214883+iswitch1985@users.noreply.github.com>
2022-04-14 12:20:06 +0800

    Update 1-if_else.js

--- 1-if_else.js
+++ 1-if_else.js
@@ -2,7 +2,7 @@ const amount = 12
 //test-changed for mybranch1,
 // this changes are from code_copy
 //this line is coming from branch1
-//this line is add from remote123123214xxx
+//this line is add from re
 if (amount < 10) {
 console.log('small number')
 // this commented is added in VS code  jhhh

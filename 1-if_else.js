ce3af3a3773d29f77d7949680433998f07ca176d
Huang <frankie.huang@sap.com>
2022-04-14 12:00:05 +0800

    change111

--- 1-if_else.js
+++ 1-if_else.js
@@ -2,7 +2,7 @@ const amount = 12
 //test-changed for mybranch1,
 // this changes are from code_copy
 //this line is coming from branch1
-//this line is add from remote123
+//this line is add from remote123123214xxx
 if (amount < 10) {
 console.log('small number')
 }
